4ca6526ae74f6bf228201b9170de0a9723e977be
IlyRov <127045335+IlyRov@users.noreply.github.com>
2024-03-08 17:57:33 +0000

    update

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,9 @@
-let some:unknown;
+let some: unknown;
 some = 'Text';
-let str: string;
-str = some;
 
-export {};
+let str: string;
+if (typeof some === 'string') {
+    str = some;
+} else {
+    console.log("The type of 'some' is not string.");
+}
